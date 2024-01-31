43c7686877afaae1340b8bee0bfda973e7b45d90
tochi <tochimba26@gmail.com>
2024-01-31 19:47:36 +0000

    Add project files.

---
+++ ToDoList/ListItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+
+    internal class ListItem
+    {
+        private string task;
+        private DateTime date;
+        private int index;
+        private bool complete;
+
+        public ListItem(string aTask, DateTime aDate, int aIndex, bool aComplete)
+        {
+            Task = aTask;
+            Date = aDate;
+            Index = aIndex;
+            complete = aComplete;
+        }
+
+        public string Task
+        {
+            get { return task; }
+            set { task = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public int Index
+        {
+            get { return index; }
+            set
+            {
+                try
+                {
+                    Convert.ToInt32(value);
+
+                    index = value;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public bool Complete
+        {
+            get { return complete; }
+            set { complete = value; }
+        }
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,70 @@
+﻿
+
+using ToDoList;
+
+
+List<ListItem> listItems = new List<ListItem>();
+
+static bool AddItem(ListItem newItem, List<ListItem> listItems)
+{
+
+    bool added = false;
+
+
+    newItem.Index = listItems.Count;
+    listItems.Add(newItem);
+    added = true;
+
+    return added;
+
+}
+
+static bool RemoveItem(int aIndex, List<ListItem> listItems)
+{
+    listItems.RemoveAt(aIndex);
+    return true;
+}
+ 
+
+static bool GetAllItems(List<ListItem> listItems)
+{
+    Console.WriteLine("--|completed|task|date|--");
+    string complete;
+    foreach (var item in listItems)
+    {
+        if (item.Complete)
+        {
+            complete = "✔️";
+        }
+        else
+        {
+            complete = "no";
+        }
+        Console.WriteLine("-------------------------");
+        Console.WriteLine(" "+item.Index+"|"+complete+"|"+item.Task+"|"+ item.Date.ToString()+"|");
+    }
+    return true;
+}
+
+
+
+
+string task;
+DateTime date = DateTime.Now;
+
+Console.Write("Enter your task: ");
+task = Console.ReadLine();
+
+ListItem newItem = new ListItem(task, date, 0, false);
+
+if (AddItem(newItem, listItems))
+{
+    Console.WriteLine("added new task to list!");
+    
+}
+
+GetAllItems(listItems);
+RemoveItem(0, listItems);
+GetAllItems(listItems);
+
+
